d45fabfe936f5b1049d06b6f93ed3d87c173f751
Rishabh-Trivedi7 <triverisha@gmail.com>
2026-05-18 20:00:51 +0530

    Final Version of Landing Page.

--- Task1_Landing_Page/script.js
+++ Task1_Landing_Page/script.js
@@ -1,6 +1,37 @@
-const menuBtn = document.querySelector('.menu-btn');
-const navLinks = document.querySelector('.nav-links');
+const menuBtn = document.querySelector(".menu-btn");
 
-menuBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+
+/* ================= DROPDOWN ================= */
+
+const dropdown = document.querySelector(".dropdown");
+
+const exploreToggle = document.querySelector("#exploreToggle");
+
+exploreToggle.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    dropdown.classList.toggle("active");
+
+});
+
+
+/* CLOSE DROPDOWN WHEN CLICKING OUTSIDE */
+
+document.addEventListener("click", (e) => {
+
+    if(!dropdown.contains(e.target)){
+
+        dropdown.classList.remove("active");
+
+    }
+
 });
